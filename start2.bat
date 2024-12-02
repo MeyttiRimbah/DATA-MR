9a108f5978419621accb32d59ebda47db7468638
MeyttiRimbah <120246724+MeyttiRimbah@users.noreply.github.com>
2024-12-02 20:13:13 +0700

    Update start2.bat

--- start2.bat
+++ start2.bat
@@ -1,2 +1,3 @@
+@echo off
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl http://localhost:4040/api/tunnels/ | jq ".tunnels[0].public_url" || echo "Failed to retreive NGROK authtoken - check again your authtoken"
