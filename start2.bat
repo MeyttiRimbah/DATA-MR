caa3f56872db95110c64c5284b2053c157f90dc2
MeyttiRimbah <120246724+MeyttiRimbah@users.noreply.github.com>
2024-12-13 07:10:27 +0700

    Update start2.bat

--- start2.bat
+++ start2.bat
@@ -1,33 +1,3 @@
 @echo off
-setlocal enabledelayedexpansion
-
-:: Display message
 echo IP:
-
-:: Check if ngrok.exe is running
-tasklist | find /i "ngrok.exe" >nul
-if errorlevel 1 (
-    echo "Ngrok is not running or not found."
-    exit /b
-)
-
-:: Fetch the public URL from the Ngrok API
-curl -s http://localhost:4040/api/tunnels/ > ngrok_response.json
-
-:: Parse the public URL from the JSON response
-set public_url=
-for /f "tokens=2 delims=:," %%A in ('findstr /i "public_url" ngrok_response.json') do (
-    set temp_url=%%A
-    set temp_url=!temp_url:"=!
-    set public_url=!temp_url!
-)
-
-:: Clean up temporary file
-del ngrok_response.json
-
-:: Check if the public URL is retrieved
-if "!public_url!"=="" (
-    echo "Failed to retrieve the Ngrok endpoint - check again your Ngrok configuration."
-) else (
-    echo tcp://!public_url!
-)
+tasklist | find /i "ngrok.exe" >Nul && curl -s http://localhost:4040/api/tunnels/ | jq ".tunnels[0].public_url" || echo "Failed to retreive NGROK authtoken - check again your authtoken"
